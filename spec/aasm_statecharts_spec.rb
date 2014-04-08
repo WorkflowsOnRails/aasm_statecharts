1f39faeadf0c4cfc3ff43c7184016123b46efe89
Brendan MacDonell <brendan@macdonell.net>
2014-04-07 21:17:17 -0400

    Finish test coverage for the rendering logic

--- spec/aasm_statecharts_spec.rb
+++ spec/aasm_statecharts_spec.rb
@@ -1,7 +1,10 @@
-#
+# Unit tests for aasm_statecharts. All checks are performed against
+# the representation held by Ruby-Graphviz, not the files written
+# to disk; we're dependent on Ruby-Graphviz and dot getting it right.
 #
 # @author Brendan MacDonell
 
+require 'fileutils'
 require 'spec_helper'
 
 class NoAasm
@@ -22,11 +25,33 @@ class SingleState < ActiveRecord::Base
   end
 end
 
-def name_of(s)
-  s.gsub(/\A"|"\Z/, '')
+class ManyStates < ActiveRecord::Base
+  include AASM
+
+  aasm do
+    state :a, initial: true, exit: :a_exit
+    state :b, enter: :b_enter
+    state :c, final: true
+
+    event :x do
+      transitions from: :a, to: :a, guard: :x_guard
+      transitions from: :b, to: :c
+    end
+
+    event :y do
+      transitions from: :a, to: :b, on_transition: :y_action
+    end
+
+    event :z do
+      transitions from: :b, to: :a, on_transition: [:z1, :z2]
+    end
+  end
 end
 
+
 describe AasmStatechart do
+  include SpecHelper
+
   it 'fails when given a class that does not have aasm included' do
     expect { AasmStatechart::Renderer.new(NoAasm) }.to raise_error
   end
@@ -35,16 +60,68 @@ describe AasmStatechart do
     expect { AasmStatechart::Renderer.new(EmptyAasm) }.to raise_error
   end
 
+  it 'fails if an invalid file format is given' do
+    renderer = AasmStatechart::Renderer.new(SingleState)
+
+    Dir.mktmpdir do |dir|
+      filename = "#{dir}/single.png"
+      expect { renderer.save(filename, format: 'foobar') }.to raise_error
+    end
+  end
+
   it 'renders statecharts with single states' do
     renderer = AasmStatechart::Renderer.new(SingleState)
     edges = renderer.graph.each_edge
     nodes = renderer.graph.each_node
 
     expect(edges.length).to equal 1
+    expect(nodes.length).to equal 2
+
     expect(name_of(edges[0].node_one)).to eq name_of(renderer.start_node.id)
     expect(edges[0].node_two).to eq "single"
 
-    expect(nodes['single']['label'].source).to match(/entry \/ foo\(\); bar\(\);/)
-    expect(nodes['single']['label'].source).to match(/exit \/ baz\(\); quux\(\);/)
+    expect_label_matches(nodes['single'], /entry \/ foo\(\); bar\(\);/)
+    expect_label_matches(nodes['single'], /exit \/ baz\(\); quux\(\);/)
+
+    Dir.mktmpdir do |dir|
+      filename = "#{dir}/single.png"
+      FileUtils.touch(filename)
+      renderer.save(filename, format: 'png')
+      expect(File.exists?(filename)).to be true
+    end
+  end
+
+  it 'renders statecharts of arbitrary complexity' do
+    renderer = AasmStatechart::Renderer.new(ManyStates)
+
+    edges = renderer.graph.each_edge
+    nodes = renderer.graph.each_node
+
+    expect(edges.length).to equal 6
+    expect(nodes.length).to equal 5
+
+    find_edge(edges, renderer.start_node.id, 'a')
+    find_edge(edges, 'c', renderer.end_node.id)
+
+    a_a = find_edge(edges, 'a', 'a')
+    expect_label_matches(a_a, /x \[x_guard\]/)
+
+    a_b = find_edge(edges, 'a', 'b')
+    expect_label_matches(a_b, /y \/ y_action\(\);/)
+
+    b_a = find_edge(edges, 'b', 'a')
+    expect_label_matches(b_a, /z \/ z1\(\); z2\(\);/)
+
+    b_c = find_edge(edges, 'b', 'c')
+    expect_label_matches(b_c, /x/)
+
+    expect_label_matches(nodes['a'], /exit \/ a_exit/)
+    expect_label_matches(nodes['b'], /entry \/ b_enter/)
+
+    Dir.mktmpdir do |dir|
+      filename = "#{dir}/many.png"
+      renderer.save(filename, format: 'png')
+      expect(File.exists?(filename)).to be true
+    end
   end
 end
