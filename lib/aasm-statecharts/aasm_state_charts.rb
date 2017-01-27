36a1c5e1cb34f5d4a3457eec9861f85f6bafe24f
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2017-01-27 00:00:52 -0800

    refactor: pull graphviz options out

--- lib/aasm-statecharts/aasm_state_charts.rb
+++ lib/aasm-statecharts/aasm_state_charts.rb
@@ -5,6 +5,7 @@ require 'rails'
 require_relative 'transition_table'
 require_relative 'chart_renderer'
 require_relative 'errors'
+require_relative 'graphviz_options'
 
 require 'pp'
 
@@ -37,16 +38,6 @@ module AASM_StateChart
 
   class AASM_StateCharts
 
-    ALL_ATTRIBS_TITLE = 'All attribute groups'
-    GRAPH_ATTRIBS_TITLE = 'Graph attributes:'
-    NODE_ATTRIBS_TITLE = 'Node attributes:'
-    GV_TYPES_ATTRIBS_TITLE = 'GraphViz types:'
-    EDGE_ATTRIBS_TITLE = 'Edge attributes:'
-    COLORS_ATTRIBS_TITLE = 'Colors:'
-    PROGRAMS_ATTRIBS_TITLE = 'Programs:'
-    GRAPHTYPE_ATTRIBS_TITLE = 'Graph Types:'
-    FORMAT_ATTRIBS_TITLE = 'File Formats:'
-
 
     def initialize(options={})
 
@@ -75,14 +66,14 @@ module AASM_StateChart
 
       @config_options = Hash.new.merge(load_config_file(options.fetch(:config_file, '')))
 
-
     end
 
 
     def run
 
       if @options.fetch(:dump_configs, false)
-        dump_attribs @options[:dump_configs]
+        output = GraphvizOptions.dump_attribs @options[:dump_configs]
+        puts output
       end
 
       unless @models.blank?
@@ -117,7 +108,7 @@ module AASM_StateChart
 
 
           rescue => e
-            raise AASM_NotFound_Error, "\nERROR: Could not find the model in #{m}.\n"
+            raise  e #AASM_NotFound_Error, "\nERROR: Could not find the model in #{m}.\n"
           end
         end
 
@@ -128,87 +119,6 @@ module AASM_StateChart
 
     # - - - - - - - -
     private
-
-    def dump_attribs(option)
-
-      gv_colors = {'named colors': GraphViz::Utils::Colors::COLORS}
-      gv_formats = {'output file formats': GraphViz::Constants::FORMATS}
-      gv_programs = {'programs to process dot file': GraphViz::Constants::PROGRAMS}
-      gv_graphtype = {'graph types': GraphViz::Constants::GRAPHTYPE}
-
-      # @url http://www.graphviz.org/content/arrow-shapes
-      #gv_arrowshapes = {'Shapes': ['box', 'crow', 'curve', 'icurve', 'diamond', 'dot', 'inv', 'none', 'normal', 'tee', 'vee'],
-      #                  'modifiers': ['o', 'l', 'r']}
-
-      # @url http://www.graphviz.org/content/color-names
-      #gv_color_schemes = {}
-
-      # TODO friendlize: put into the format for config.yml.  Explain the keys and some values
-
-      # node shapes and info @url http://www.graphviz.org/content/node-shapes
-
-      #gv_types_info = {
-      #    EscString: 'a string',
-      #    GvDouble: 'decimal point number (double)',
-      #    GvBool: 'boolean (true or false)',
-      #    Color: "one of the accepted color names, or the hex code WITHOUT the '#'",
-      #    ArrowType: "one of the arrow shapes with optional modifiers",
-      #    Rect: "???",
-      #    SplineType: "???",
-      #    LblString: "??? label string?  <gv>STRING</gv>",
-      #    HtmlString: " a string surrounded by '<' and '>' (prepended with '<' and appended with '>')"
-      #}
-
-
-      config_hash = case option
-
-                      when :all
-                        title_with_hash ALL_ATTRIBS_TITLE,
-                                        graphviz_options_for(/G|S|X/)
-                                            .merge(graphviz_options_for(/N/))
-                                            .merge(graphviz_options_for(/E/))
-                                            .merge(gv_colors)
-                                            .merge({'Formats': gv_formats})
-                                            .merge({'Programs': gv_programs})
-                                            .merge({'Graph Types': gv_graphtype})
-
-                      when :graph
-                        title_with_hash GRAPH_ATTRIBS_TITLE, graphviz_options_for(/G|S|X/)
-
-                      when :nodes
-                        title_with_hash NODE_ATTRIBS_TITLE, graphviz_options_for(/N/)
-
-                      when :edges
-                        title_with_hash EDGE_ATTRIBS_TITLE, graphviz_options_for(/E/)
-
-                      when :colors
-                        title_with_hash COLORS_ATTRIBS_TITLE, gv_colors
-
-                      when :formats
-                        title_with_hash FORMAT_ATTRIBS_TITLE, gv_formats # Array
-
-                      when :programs
-                        title_with_hash PROGRAMS_ATTRIBS_TITLE, gv_programs # Array
-
-                      when :graphtype
-                        title_with_hash GRAPHTYPE_ATTRIBS_TITLE, gv_graphtype # Array
-                      else
-                        {}
-                    end
-
-      puts config_hash.pretty_inspect
-      config_hash
-    end
-
-
-    def graphviz_options_for(regex)
-      GraphViz::Constants::getAttrsFor(regex)
-    end
-
-
-    def title_with_hash(title, hash)
-      {'Title': title, 'values': hash}
-    end
 
 
     def get_included_paths(options_path)
@@ -261,18 +171,6 @@ module AASM_StateChart
     end
 
 
-    def try_requires(method, model)
-      success = false
-      begin
-        Kernel.send method, model
-        success = true
-      rescue => e
-        # no op
-      end
-      success
-    end
-
-
     def get_models(all_option, models)
 
       model_classes = []
@@ -291,38 +189,14 @@ module AASM_StateChart
 
 
           unless found_model
-            # TODO - don't continue to iterate if it's found
-            @include_paths.each do |fullpath|
 
-              found_model = require_or_load(model, File.join(fullpath, fname) )
+            i = 0
 
-=begin
-              if File.exist?(File.join(fullpath, fname))
-
-                # found the file, but if we error out, it's because we couldn't require or load it
-
-                begin
-                  require_relative File.join(fullpath, model)
-                  found_model = true
-                rescue
-                  #no op
-                  puts "could not require_relative #{File.join(fullpath, model)}"
-                end
-
-                unless found_model
-                  begin
-                    load File.join(fullpath, fname)
-                    found_model = true
-                  rescue
-                    # no op
-                    puts "could not load #{File.join(fullpath, fname)}"
-                  end
-                end
-
-              end
-=end
-
+            while !found_model && i < @include_paths.size
+              found_model = require_or_load(model, File.join(@include_paths[i], fname) )
+              i += 1
             end
+
           end
 
 
@@ -383,29 +257,15 @@ module AASM_StateChart
     end
 
 
-    def load_models(load_all, models_named)
-
-      @models = load_all ? collect_all_models : load_models_named(models_named)
-
-    end
-
-
-    def load_models_named(model_names)
-      model_names.map do |model_name|
-
-        # must strip off any leading path, add that path to the $Loadpath, and then set the model to just the filename
-        just_model = File.basename model_name
-
-        just_model.camelize.constantize
+    def try_requires(method, model)
+      success = false
+      begin
+        Kernel.send method, model
+        success = true
+      rescue => e
+        # no op
       end
-    end
-
-
-    def collect_all_models
-
-      Rails::Application.subclasses.first.eager_load!
-      ActiveRecord::Base.descendants.select { |klass| klass.respond_to? :aasm }
-
+      success
     end
 
 
@@ -454,6 +314,36 @@ module AASM_StateChart
     def symbolize_the_keys(hash)
 
       hash.each_with_object({}) { |(k, v), h| h[k.to_sym] = v.is_a?(Hash) ? symbolize_the_keys(v) : v }
+    end
+
+
+    # - - - - - -
+    # old cruft
+    def load_models(load_all, models_named)
+
+      @models = load_all ? collect_all_models : load_models_named(models_named)
+
+    end
+
+
+    # old cruft
+    def load_models_named(model_names)
+      model_names.map do |model_name|
+
+        # must strip off any leading path, add that path to the $Loadpath, and then set the model to just the filename
+        just_model = File.basename model_name
+
+        just_model.camelize.constantize
+      end
+    end
+
+
+    # old cruft
+    def collect_all_models
+
+      Rails::Application.subclasses.first.eager_load!
+      ActiveRecord::Base.descendants.select { |klass| klass.respond_to? :aasm }
+
     end
 
 
